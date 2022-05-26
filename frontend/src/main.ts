b4fc28a4721e04c0fbc11ab79affd9cd77fb7d11
Guillaume LADORME <gladorme@gmail.com>
2022-05-26 13:54:42 +0200

    Add Devtools + Vuex + Prettier :pig:

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,10 @@
 import {createApp} from 'vue'
 import App from './App.vue'
+import devtools from '@vue/devtools'
+import { store, key } from './store'
 
-createApp(App).mount('#app')
+createApp(App).use(store, key).mount('#app')
+if (process.env.NODE_ENV === 'development') {
+    devtools.connect(/* host, port */)
+}
+  
